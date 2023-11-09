3f4eab58c7398bb37b3e0984a4c812221198cde1
Akakira21 <kenzo@prouveze.fr>
2023-11-09 16:18:48 +0100

    AdminPanel addSerie front, back à faire

--- serie-front/src/apis/series.js
+++ serie-front/src/apis/series.js
@@ -1,3 +1,24 @@
+export async function addSerie(values) {
+  try {
+    const response = await fetch("http://localhost:8000/addSerie", {
+      method: "POST",
+      body: values,
+    });
+    const body = await response.json();
+    if (response.ok) {
+      return body;
+    } else {
+      if (body) {
+        throw body;
+      } else {
+        throw new Error("Error register");
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
 export async function getSeries(id) {
     const response = await fetch(`http://localhost:8000/getSeries`);
     return response.json();
